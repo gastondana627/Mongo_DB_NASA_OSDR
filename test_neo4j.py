0be9038e8bc1da5d4cecb2599520d527fdbd07ec
GasMan <47839532+gastondana627@users.noreply.github.com>
2025-08-22 04:24:22 -0500

    Update test_neo4j.py

--- test_neo4j.py
+++ test_neo4j.py
@@ -3,9 +3,9 @@
 from neo4j import GraphDatabase
 
 # --- PASTE YOUR CREDENTIALS DIRECTLY HERE ---
-NEO4J_URI="neo4j+s://5be416cb.databases.neo4j.io"
+NEO4J_URI="__"
 NEO4J_USER="neo4j"
-NEO4J_PASSWORD="iyODr6mTbLqK5wpj2eKE2ZUbTQbz3-DnZ6kTLiNvZPM"
+NEO4J_PASSWORD="__"
 
 # ---------------------------------------------
 
